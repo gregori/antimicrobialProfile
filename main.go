232fcaa593b64db04d89e6f3ea2834bc572eface
Rodrigo Habib Gregori <spam@gregori.eti.br>
2020-01-03 16:33:05 -0300

    add initial service entrypoint

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// Index root function handler
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
